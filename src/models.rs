074278b92699efbe9bea7b40bb8ee3987d01d596
Vitaliy Busko <vitaliy.opensource@gmail.com>
2026-03-14 14:50:34 +0700

    feat: add basic support to generate query-parameter models
     - related issue: denislituev/openapi-model-generator/issues/53
       https://github.com/denislituev/openapi-model-generator/issues/53

--- src/models.rs
+++ src/models.rs
@@ -37,6 +37,9 @@ pub struct Field {
     pub is_required: bool,
     pub is_nullable: bool,
     pub is_array_ref: bool,
+    /// When true, emit #[serde(flatten)] for query param structs (params at same level in GET)
+    #[serde(default)]
+    pub flatten: bool,
     pub description: Option<String>,
     /// Field-level Rust attributes from x-rust-attrs (e.g. #[serde(rename = "...")])
     #[serde(default)]
@@ -44,9 +47,9 @@ pub struct Field {
 }
 
 impl Field {
-    /// Returns true if this field should be flattened (for additionalProperties)
+    /// Returns true if this field should be flattened (for additionalProperties or query params)
     pub fn should_flatten(&self) -> bool {
-        self.name == "additional_properties"
+        self.name == "additional_properties" || self.flatten
     }
 }
 
